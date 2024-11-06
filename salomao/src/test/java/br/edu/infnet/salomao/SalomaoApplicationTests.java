2b095225566450489525770d75390eabf872396d
SalNet <salomaonasser@gmail.com>
2024-11-05 23:58:58 -0300

    Atualizando o repositório com a versão final do app

--- salomao/src/test/java/br/edu/infnet/salomao/SalomaoApplicationTests.java
+++ salomao/src/test/java/br/edu/infnet/salomao/SalomaoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ElberthApplicationTests {
+class SalomaoApplicationTests {
 
 	@Test
 	void contextLoads() {
